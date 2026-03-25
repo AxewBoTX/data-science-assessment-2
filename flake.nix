46c349cf8e73aceae0c960ce0f51b8c0fbf8e7a2
lovedeepsingh-07 <lovedeepsinghbrar07@gmail.com>
2026-03-25 22:47:46 +1100

    beginning of the end

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "data-science-assessment-2";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/ac055f38c798b0d87695240c7b761b82fc7e5bc2";
+  };
+  outputs = {...} @ inputs: let
+    system = "x86_64-linux";
+    pkgs = import inputs.nixpkgs {
+      inherit system;
+    };
+    python = rec {
+      version = "311";
+      pkg = pkgs."python${version}";
+      env = pkg.withPackages (pp:
+        with pp; [
+          numpy
+          matplotlib
+		  pandas
+		  ipykernel
+		  jupyter-server
+		  scikit-learn
+        ]);
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = [
+        pkgs.alejandra
+        pkgs.pkg-config
+        python.env
+        pkgs.black
+      ];
+      buildInputs = [];
+      shellHook = "zsh";
+    };
+  };
+}
---
+++ justfile
@@ -0,0 +1,6 @@
+jupyter:
+	jupyter server \
+	  --IdentityProvider.token='' \
+	  --ServerApp.password='' \
+	  --ServerApp.disable_check_xsrf=True \
+	  --ServerApp.allow_origin='*'
